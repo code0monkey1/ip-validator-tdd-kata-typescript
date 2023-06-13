d9ef69f7421ff25f99270c4f2fb8852cc7c3a45f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-13 06:01:56 +0000

    13 Jun 2023, 11:31

--- test/IpValidator.test.ts
+++ test/IpValidator.test.ts
@@ -3,7 +3,13 @@ import IpValidator from "../src/IpValidator"
 describe('IpValidator', () => {
        
       it('is defined',()=>{
-
+           
            expect(IpValidator).toBeDefined()
+
+           const ipValidator = new IpValidator()
+
+           expect(ipValidator.validateIpv4Address).toBeDefined()
+
+           expect(ipValidator.validateIpv4Address).toBeInstanceOf(Function)
       })
 })
